d05436b498ad0f459b9b3f995f4302eab1f03a0c
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2021-03-21 15:02:05 -0400

    Comments and Cleaned Code

--- YouTube.py
+++ YouTube.py
@@ -1,13 +1,18 @@
+# Retrieves relevant data from the YouTube search history or watch history .html files of the Google Takeout .zip file.
+
 import urllib.parse
 import html
 
+# Returns a list of string data points from the search-history.html file of a user's YouTube data.
 def searchHistory(filePath):
+    # Getting the important data from the bottom of the html file.
     with open(filePath, encoding="utf8") as file:
         raw = file.readline()
         while raw[:8] != "</style>":
             raw = file.readline()
         file.close()
 
+    # Splitting the html file to get the search data, raw looks like: '...query=search+text+here"...'
     queries = raw.split("query=")
     matchable = []
     for query in queries:
@@ -17,6 +22,7 @@ def searchHistory(filePath):
 
     return matchable[1:]
 
+# Returns a list of string data points from the watch-history.html file of a user's YouTube data.
 def watchHistory(filePath):
     with open(filePath, encoding="utf8") as file:
         raw = file.readline()
@@ -24,6 +30,7 @@ def watchHistory(filePath):
             raw = file.readline()
         file.close()
 
+    # Splitting the html file to get the video title, raw looks like: '...">video title here</a>...'
     queries = raw.split("</a>")
     matchable = []
     for query in queries:
